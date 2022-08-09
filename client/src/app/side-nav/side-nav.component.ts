4296f4ccae9a9fc10e5da5a37c133e72df7970ba
stevenquintana <steve.e.quint@gmail.com>
2022-08-09 09:22:03 -0700

    Create a side nav component

---
+++ client/src/app/side-nav/side-nav.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+/**
+ * @title SideNav
+ */
+@Component( {
+    selector: 'side-nav',
+    templateUrl: 'side-nav.component.html',
+    styleUrls: ['side-nav.component.css'],
+  }
+
+)
+
+export class SideNavComponent {
+  showFiller=false;
+}
