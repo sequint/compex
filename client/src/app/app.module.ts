19c9f5ee7c4186f81d10eb3fd2bdec827bcea8c6
stevenquintana <steve.e.quint@gmail.com>
2022-08-09 09:25:40 -0700

    Add Matsidenavmodule to imports

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 import { AppComponent } from './app.component';
 import { BottomNavComponent } from './bottom-nav/bottom-nav.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    BottomNavComponent
+    BottomNavComponent,
+    SideNavComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
