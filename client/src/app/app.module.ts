69f874c11b183205763cad2b46943ac03a3e9f5f
stevenquintana <steve.e.quint@gmail.com>
2022-08-12 09:25:05 -0700

    Delete bottom nav component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
-import { BottomNavComponent } from './bottom-nav/bottom-nav.component';
 import { SideNavComponent } from './side-nav/side-nav.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BottomNavComponent,
     SideNavComponent
   ],
   imports: [
