8187464824bf02414d7027cf2aef51c9270e0e7e
stevenquintana <steve.e.quint@gmail.com>
2022-08-12 14:52:54 -0700

    Update list spacing for side menu

--- client/src/app/side-nav/side-nav.component.ts
+++ client/src/app/side-nav/side-nav.component.ts
@@ -3,19 +3,18 @@ import { Component } from '@angular/core';
 /**
  * @title SideNav
  */
-@Component( {
+@Component({
     selector: 'side-nav',
     templateUrl: 'side-nav.component.html',
     styleUrls: ['side-nav.component.css'],
   }
-
 )
 
 export class SideNavComponent {
   private menuOpen: boolean;
   private openClass: string;
   private closedClass: string;
-  
+
   menuItems: string[];
 
   constructor() {
