c0dd0f641cd4c87a25384ef03151bf028020bf89
stevenquintana <steve.e.quint@gmail.com>
2022-08-15 14:51:02 -0700

    Create skeleton comp for exchange grid

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeTitleComponent } from './home-title/home-title.component';
+import { ExchangeGridComponent } from './exchange-grid/exchange-grid.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    HomeTitleComponent
+    HomeTitleComponent,
+    ExchangeGridComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/exchange-grid/exchange-grid.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+/**
+ * @title ExchangeGrid
+ */
+@Component({
+    selector: 'exchange-grid',
+    templateUrl: './exchange-grid.component.html',
+    styleUrls: ['./exchange-grid.component.css'],
+  }
+)
+
+export class ExchangeGridComponent {}
