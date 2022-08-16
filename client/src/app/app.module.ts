3e59d7f4e2e21a010c05b760a198c33a602ff1a8
stevenquintana <steve.e.quint@gmail.com>
2022-08-16 15:47:38 -0700

    Delete search as its own comp and place in ex-grid

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeTitleComponent } from './home-title/home-title.component';
-import { SearchBarComponent } from './search-bar/search-bar.component';
 import { ExchangeGridComponent } from './exchange-grid/exchange-grid.component';
 import { CompostDividerComponent } from './compost-divider/compost-divider.component';
 
@@ -15,7 +14,6 @@ import { CompostDividerComponent } from './compost-divider/compost-divider.compo
     AppComponent,
     NavBarComponent,
     HomeTitleComponent,
-    SearchBarComponent,
     ExchangeGridComponent,
     CompostDividerComponent
   ],
