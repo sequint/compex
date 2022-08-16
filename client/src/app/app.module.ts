aa8b82575e81fe86e08a873fe1d7e07ff830edc6
stevenquintana <steve.e.quint@gmail.com>
2022-08-16 14:47:33 -0700

    Create a search bar comp to search through compx items

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { HomeTitleComponent } from './home-title/home-title.component';
+import { SearchBarComponent } from './search-bar/search-bar.component';
 import { ExchangeGridComponent } from './exchange-grid/exchange-grid.component';
 import { CompostDividerComponent } from './compost-divider/compost-divider.component';
 
@@ -14,6 +15,7 @@ import { CompostDividerComponent } from './compost-divider/compost-divider.compo
     AppComponent,
     NavBarComponent,
     HomeTitleComponent,
+    SearchBarComponent,
     ExchangeGridComponent,
     CompostDividerComponent
   ],
---
+++ client/src/app/search-bar/search-bar.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+/**
+ * @title SearchBar
+ */
+@Component({
+    selector: 'search-bar',
+    templateUrl: './search-bar.component.html',
+    styleUrls: ['./search-bar.component.css'],
+  }
+)
+
+export class SearchBarComponent {}
