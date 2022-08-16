045e987b115e50241c5b2dd90330872e1d24b96d
stevenquintana <steve.e.quint@gmail.com>
2022-08-16 08:47:31 -0700

    Create compx items array with getters and setters

--- client/src/app/exchange-grid/exchange-grid.component.ts
+++ client/src/app/exchange-grid/exchange-grid.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CompxItem } from 'src/interfaces/CompxItem';
 
 /**
  * @title ExchangeGrid
@@ -10,4 +11,29 @@ import { Component } from '@angular/core';
   }
 )
 
-export class ExchangeGridComponent {}
+export class ExchangeGridComponent {
+  compxItems: CompxItem[];
+
+  constructor() {
+    this.compxItems = [{
+      name: 'Banana Scraps',
+      icon: '../../assets/icons/banana.png',
+      value: 4.05,
+      valChange: 20.00,
+      arrowIcon: ''
+    }];
+
+    this.setArrowIcon();
+  }
+
+  setArrowIcon = () => {
+    if (this.compxItems[0].valChange > 0) { this.compxItems[0].arrowIcon = 'gg-arrow-up'}
+    else if (this.compxItems[0].valChange < 0) { this.compxItems[0].arrowIcon = 'gg-arrow-down'};
+  }
+
+  getItemName = () => this.compxItems[0].name;
+  getItemIcon = () => this.compxItems[0].icon;
+  getItemValue = () => this.compxItems[0].value;
+  getItemValChange = () => this.compxItems[0].valChange;
+  getArrowIcon = () => this.compxItems[0].arrowIcon;
+}
