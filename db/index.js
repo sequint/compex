87a94983c48e4690e164889dc51f987fca514373
stevenquintana <steve.e.quint@gmail.com>
2022-09-08 15:31:04 -0700

    Add missing semi-colons

---
+++ db/index.js
@@ -0,0 +1,6 @@
+const mongoose = require('mongoose');
+
+// Export an async function that tried to connect to foreign Mongo server, and conencts to local server if that fails.
+module.exports = async function syncDB () { 
+  await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/rejector_db');
+}
