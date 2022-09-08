c7d325e4a33588291efda2e5a7a66aa3c62c82b2
stevenquintana <steve.e.quint@gmail.com>
2022-09-08 15:33:33 -0700

    Create index db to connect to db using mongoose

--- db/index.js
+++ db/index.js
@@ -2,5 +2,5 @@ const mongoose = require('mongoose');
 
 // Export an async function that tried to connect to foreign Mongo server, and conencts to local server if that fails.
 module.exports = async function syncDB () { 
-  await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/rejector_db');
+  await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/compx_db');
 }
