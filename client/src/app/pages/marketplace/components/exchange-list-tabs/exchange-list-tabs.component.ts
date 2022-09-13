4dbf59c1b002650dbce5bd877996303de5eeaac5
stevenquintana <steve.e.quint@gmail.com>
2022-09-13 12:42:42 -0700

    Create component for exchange list tabs

---
+++ client/src/app/pages/marketplace/components/exchange-list-tabs/exchange-list-tabs.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+/**
+ * @title ExchangeListTabs
+ */
+@Component({
+    selector: 'exchange-list-tabs',
+    templateUrl: './exchange-list-tabs.component.html',
+    styleUrls: ['./exchange-list-tabs.component.css'],
+  }
+)
+
+export class ExchangeListTabs {
+  
+}
--- client/src/app/pages/marketplace/marketplace.module.ts
+++ client/src/app/pages/marketplace/marketplace.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ExchangeListComponent } from './components/exchange-list/exchange-list.component';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ExchangeListTabs } from './components/exchange-list-tabs/exchange-list-tabs.component';
 
 
 @NgModule({
   declarations: [
+    ExchangeListTabs,
     ExchangeListComponent
   ],
   imports: [
@@ -13,6 +15,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
     SharedModule
   ],
   exports: [
+    ExchangeListTabs,
     ExchangeListComponent
   ]
 })
