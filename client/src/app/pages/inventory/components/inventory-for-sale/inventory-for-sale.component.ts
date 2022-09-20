ee930c85a115367e334cd6d1501a392688d04a42
stevenquintana <steve.e.quint@gmail.com>
2022-09-20 10:23:53 -0700

    Initialize inventory-for-sale component

---
+++ client/src/app/pages/inventory/components/inventory-for-sale/inventory-for-sale.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'inventory-for-sale',
+    templateUrl: 'inventory-for-sale.component.html',
+    styleUrls: ['inventory-for-sale.component.scss']
+})
+export class InventoryForSaleComponent {
+
+}
