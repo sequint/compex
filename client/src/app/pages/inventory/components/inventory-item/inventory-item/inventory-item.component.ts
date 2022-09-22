1e8619fc9dca098f65d667bfe383adba163bcc64
stevenquintana <steve.e.quint@gmail.com>
2022-09-22 07:09:29 -0700

    Initialize inventory item component

---
+++ client/src/app/pages/inventory/components/inventory-item/inventory-item/inventory-item.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'inventory-item',
+    templateUrl: 'inventory-item.component.html',
+    styleUrls: ['inventory-item.component.scss']
+})
+export class InventoryItemComponent {
+
+}
