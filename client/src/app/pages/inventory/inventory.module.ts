2ac604b86ae802efd4fe8df77774b2a09bb7ae0a
stevenquintana <steve.e.quint@gmail.com>
2022-09-23 15:19:29 -0700

    Delete old item component and replace with new

--- client/src/app/pages/inventory/inventory.module.ts
+++ client/src/app/pages/inventory/inventory.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { InventoryListComponent } from './components/inventory-list/inventory-list.component';
+import { InventoryItemComponent } from './components/inventory-item/inventory-item.component';
 
 
 @NgModule({
   declarations: [
-    InventoryListComponent
+    InventoryListComponent,
+    InventoryItemComponent
   ],
   imports: [
     CommonModule,
