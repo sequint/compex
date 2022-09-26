15af784f80cb1b223d8c816b2e9bef5737ad1e4e
stevenquintana <steve.e.quint@gmail.com>
2022-09-26 16:25:14 -0700

    Add passport functions back to the server

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 require('dotenv').config();
 
 const express = require('express');
-// const { join } = require('path');
-// const passport = require('passport')
-// const { Strategy: LocalStrategy } = require('passport-local')
-// const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt')
+const passport = require('passport')
+const { Strategy: LocalStrategy } = require('passport-local')
+const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt')
 
-// const { User } = require('./models')
+const { User } = require('./models')
 const syncDB = require('./db');
 
 const app = express();
@@ -21,27 +20,27 @@ app.use(function (req, res, next) {
   next();
 });
 
-// // Initializing passport.
-// app.use(passport.initialize())
-// // Initialize seesion for authentication of user.
-// app.use(passport.session())
+// Initializing passport.
+app.use(passport.initialize())
+// Initialize seesion for authentication of user.
+app.use(passport.session())
 
-// // Create a local strategy to authenticate the user.
-// passport.use(new LocalStrategy(User.authenticate()))
-// // Encrypt and decrypt user information (password).
-// passport.serializeUser(User.serializeUser())
-// passport.deserializeUser(User.deserializeUser())
+// Create a local strategy to authenticate the user.
+passport.use(new LocalStrategy(User.authenticate()))
+// Encrypt and decrypt user information (password).
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
 
-// // Compare web token to secret on .env file to authenticate user.
-// passport.use(new JwtStrategy({
-//   // Take token from the bearer header information passed into axios request for token.
-//   jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-//   // Set var for secret.
-//   secretOrKey: process.env.SECRET
-// }, ({ id }, cb) => User.findById(id)
-//   .populate('jobs')
-//   .then(user => cb(null, user))
-//   .catch(err => cb(err))))
+// Compare web token to secret on .env file to authenticate user.
+passport.use(new JwtStrategy({
+  // Take token from the bearer header information passed into axios request for token.
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  // Set var for secret.
+  secretOrKey: process.env.SECRET
+}, ({ id }, cb) => User.findById(id)
+  .populate('jobs')
+  .then(user => cb(null, user))
+  .catch(err => cb(err))))
 
 app.use(require('./routes'));
 
