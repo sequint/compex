00f5dc5ca37776f583a216ece00011415220f1de
stevenquintana <steve.e.quint@gmail.com>
2022-09-26 17:18:44 -0700

    Unistall passport

--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
-const passport = require('passport')
-const { Strategy: LocalStrategy } = require('passport-local')
-const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt')
 
-const { User } = require('./models')
 const syncDB = require('./db');
 
 const app = express();
@@ -19,28 +15,6 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
-
-// Initializing passport.
-app.use(passport.initialize())
-// Initialize seesion for authentication of user.
-app.use(passport.session())
-
-// Create a local strategy to authenticate the user.
-passport.use(new LocalStrategy(User.authenticate()))
-// Encrypt and decrypt user information (password).
-passport.serializeUser(User.serializeUser())
-passport.deserializeUser(User.deserializeUser())
-
-// Compare web token to secret on .env file to authenticate user.
-passport.use(new JwtStrategy({
-  // Take token from the bearer header information passed into axios request for token.
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  // Set var for secret.
-  secretOrKey: process.env.SECRET
-}, ({ id }, cb) => User.findById(id)
-  .populate('jobs')
-  .then(user => cb(null, user))
-  .catch(err => cb(err))))
 
 app.use(require('./routes'));
 
