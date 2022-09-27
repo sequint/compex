efb4a355c8d67bcb10c6fb827813fa5e47167d77
stevenquintana <steve.e.quint@gmail.com>
2022-09-26 19:12:29 -0700

    Style button to match site theme

--- client/src/app/shared/components/log-in-button/log-in-button.component.ts
+++ client/src/app/shared/components/log-in-button/log-in-button.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'log-in-button',
   templateUrl: './log-in-button.component.html',
-  styleUrls: ['./log-in-button.component.css']
+  styleUrls: ['./log-in-button.component.scss']
 })
 export class LogInButtonComponent implements OnInit {
 
